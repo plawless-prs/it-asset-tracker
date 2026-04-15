42e7dc8b419210cb35cdf594d8223c20e5c0e673
unknown <Porter Lawless@11-PLAWLESS.prs.local>
2026-04-15 16:18:24 -0500

    added pdf-parse to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pdfjs-dist', 'archiver'],
+  serverExternalPackages: ['pdfjs-dist', 'pdf-parse', 'archiver'],
 }
 
 export default nextConfig
